0d6861f107de979786590068b1f7e80fa62a6cee
Shashank.S <personal.shashanks@outlook.com>
2025-02-03 15:28:31 +0530

    8 different shades of color

---
+++ sangamone1/lib/screen6.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Screen6 extends StatelessWidget {
+  const Screen6({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Colorpalet(),
+    );
+  }
+}
+
+class Colorpalet extends StatefulWidget {
+  const Colorpalet({super.key});
+
+  @override
+  State<Colorpalet> createState() => _ColorpaletState();
+}
+
+class _ColorpaletState extends State<Colorpalet> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(shrinkWrap: true,itemCount: 3,itemBuilder: (context,index){
+              return Container(
+                height: 50,
+                width: MediaQuery.sizeOf(context).width,
+                child: GridView.builder(itemCount: 8,gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),itemBuilder: (context,index1){
+                  return Container(
+                    color: Color.fromRGBO(index==0?255:0, index==1?255:0, index==2?255:0, 1/(index1+1)),
+                  );
+                },),
+              );
+            }),
+          )
+        ],
+      ),
+    );
+  }
+}
+
