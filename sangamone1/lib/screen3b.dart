7b50758924fd813496aa82cb03f3ecb3de3c8f81
Shashank.S <personal.shashanks@outlook.com>
2025-02-03 14:18:10 +0530

    Modified offset in timing

--- sangamone1/lib/screen3b.dart
+++ sangamone1/lib/screen3b.dart
@@ -36,9 +36,9 @@ class _ClockincitiesbState extends State<Cites> {
           DateTime city3 = DateTime.now().toUtc().subtract(Duration(hours: 0));
           DateTime city4 = DateTime.now().toUtc().add(Duration(hours: 4));
           DateTime city5 = DateTime.now().toUtc().add(Duration(hours: 5,minutes: 30));
-          DateTime city6 = DateTime.now().toUtc().add(Duration(hours: 7,minutes: 30));
-          DateTime city7 = DateTime.now().toUtc().add(Duration(hours: 8,minutes: 30));
-          DateTime city8 = DateTime.now().toUtc().add(Duration(hours: 10,minutes: 30));
+          DateTime city6 = DateTime.now().toUtc().add(Duration(hours: 8));
+          DateTime city7 = DateTime.now().toUtc().add(Duration(hours: 9));
+          DateTime city8 = DateTime.now().toUtc().add(Duration(hours: 11));
           List<dynamic> list2 = [city1,city2,city3,city4,city5,city6,city7,city8];
           return ListView.builder(shrinkWrap: true,itemCount: list1.length,itemBuilder: (context,index){
             return Card(
