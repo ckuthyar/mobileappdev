32177005cb39b041a4b02a74d820c7cad8fe877a
Shashank.S <personal.shashanks@outlook.com>
2025-02-06 22:59:51 +0530

    hello world

--- sangamone1/lib/temp.dart
+++ sangamone1/lib/temp.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sangamone1/screen5.dart';
+import 'package:sangamone1/screen7a.dart';
+import 'package:sangamone1/screen8a.dart';
 
 class App1 extends StatelessWidget {
   const App1({super.key});
@@ -23,6 +26,10 @@ class _TempState extends State<Temp> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Text("Hello World"),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=> Colorbyvalues()));
+      },
+        child: Icon(Icons.arrow_forward_sharp),),
     );
   }
 }
