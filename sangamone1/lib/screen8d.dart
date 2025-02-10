14e9c593ad9ba54ed4602278829769f4272fa3a7
Shashank.S <personal.shashanks@outlook.com>
2025-02-10 21:10:03 +0530

    sangamone1

--- sangamone1/lib/screen8d.dart
+++ sangamone1/lib/screen8d.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sangamone1/screen8e.dart';
 
 class Screen8d extends StatelessWidget {
   const Screen8d({super.key});
@@ -30,62 +31,65 @@ class _App8dState extends State<App8d> {
   int index = 0;
   
   Map<dynamic,dynamic> correctAnswers = {};
-
   Map<dynamic,dynamic> map1 = {};
-
   TextEditingController response = TextEditingController();
 
   String Qv = "";
-
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      Question.forEach((value) => correctAnswers[value]=Answer[Question.indexOf(value)] );
+    });
+    print(correctAnswers);
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ListTile(
-              title: Text("What is the Capital of ${Question[index]}",style: TextStyle(fontSize: 30),),
-              subtitle: TextField(
-                controller: response,
-                  style: TextStyle(fontSize: 30)
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                InkWell(
-                  onTap: (){
-                    setState(() {
-                      index==0?null:index--;
-                      response.text=map1[Question[index]];
-                    });
-                  },
-                  child: Text("Previous",style: TextStyle(fontSize: 30)),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ListTile(
+                title: Text("What is the Capital of ${Question[index]}",style: TextStyle(fontSize: 30),),
+                subtitle: TextField(
+                  controller: response,
+                    style: TextStyle(fontSize: 30)
                 ),
-                index!=Question.length-1?ElevatedButton(onPressed: (){
-                  setState(() {
-                    map1[Question[index]]=response.text;
-                    index==Question.length-1?null:index++;
-                    response.text=map1[Question]==null?"":map1[Question[index]];
-                    response.clear();
-                    print(map1);
-                  });
-                }, child: Text("Next",style: TextStyle(fontSize: 30))):
-                    ElevatedButton(onPressed: (){
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  InkWell(
+                    onTap: (){
                       setState(() {
-                        Qv="Quiz Over";
+                        index==0?null:index--;
+                        response.text=map1[Question[index]];
                       });
-                    }, child: Text("Submit",style: TextStyle(fontSize: 30,color: Colors.white)),
-                    style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.blue)),
-                    ),
-              ],
-            ),
-            Text("$Qv"),
-            ListView.builder(shrinkWrap: true,itemCount: Question.length,itemBuilder: (context,index){
-              return Text("${map1[Question[index]]}");
-            })
-          ],
+                    },
+                    child: Text("Previous",style: TextStyle(fontSize: 30)),
+                  ),
+                  index!=Question.length-1?ElevatedButton(onPressed: (){
+                    setState(() {
+                      map1[Question[index]]=response.text;
+                      index==Question.length-1?null:index++;
+                      response.text=map1[Question]==null?"":map1[Question[index]];
+                      response.clear();
+                      print(map1);
+                    });
+                  }, child: Text("Next",style: TextStyle(fontSize: 30))):
+                      ElevatedButton(onPressed: (){
+                        setState(() {
+                          Qv="Quiz Over";
+                        });
+                      }, child: Text("Submit",style: TextStyle(fontSize: 30,color: Colors.white)),
+                      style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.blue)),
+                      ),
+                ],
+              ),
+              Text("$Qv"),
+              Qv==""?Container():ListView.builder(shrinkWrap: true,itemCount: Question.length,itemBuilder: (context,index){
+                return Text("${map1[Question[index]]}, ${Answer[index]}",style: TextStyle(fontSize: 26,color: map1[Question[index]]!=Answer[index]?Colors.red:Colors.black),);
+              })
+            ],
+          ),
         ),
       ),
     );
---
+++ sangamone1/lib/screen8e.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+class Screen8e extends StatelessWidget {
+  const Screen8e({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: App1(),
+    );
+  }
+}
+
+class App1 extends StatefulWidget {
+  const App1({super.key});
+
+  @override
+  State<App1> createState() => _App1State();
+}
+
+class _App1State extends State<App1> {
+
+  List<String> Question = [
+    "India", "USA", "UK", "France", "Italy", "Russia", "China", "Philippines", "Sri Lanka", "Japan"
+  ];
+  List<String> Answer = [
+    "New Delhi", "Washington DC", "London", "Paris", "Rome", "Moscow", "Beijing", "Manila", "Colombo", "Tokyo"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TableWidget(json: """[{"Marks":"10","Question":"India","Answer":"New Delhi"}]"""),
+    );
+  }
+
+  Widget TableWidget({required String json}){
+    var list1;
+    try{
+      list1=jsonDecode(json);
+      List keys = list1!.first.keys.toList();
+      List<List<dynamic>> list2 =[];
+      for (String i in keys){
+        var data = list1.map((value)=>value[i]).toList();
+        list2.add(data);
+      }
+      return GridView.builder(itemCount: keys.length+(keys.length*list1.length).toInt(),gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: keys.length,childAspectRatio: 3), itemBuilder: (context,index){
+        int value = index~/keys.length==0?index:index-keys.length;
+        return Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.black,width: 1),
+            color: index~/keys.length==0?Colors.yellow:Colors.transparent
+          ),
+          child: index~/keys.length==0?Center(child: Text("${keys[index]}",style: TextStyle(fontWeight: FontWeight.bold),)):Center(child: Text("${list2[value%keys.length][value~/keys.length]}")),
+        );
+      });
+    }
+    catch(e){
+      debugPrint("Error: Json String not correct");
+      return Center(child: Text("Error: Json String not correct"));
+    }
+  }
+}
+
+/*
+* ListView.builder(shrinkWrap: true,scrollDirection: Axis.horizontal,itemCount: 4,itemBuilder: (context,index){
+        return Column(
+          children: [
+            Container(
+              width: MediaQuery.sizeOf(context).width/4,
+              child: ListView.builder(shrinkWrap: true,itemCount: 4,itemBuilder: (context,index1){
+                return Text("data");
+              }),
+            ),
+          ],
+        );
+      })*/
+
+/*GridView.builder(itemCount: 40,gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4,childAspectRatio: 3), itemBuilder: (context,index){
+        return Container(
+            child: index%4==0?Text("${Question[index~/4]}"):index%4==1?Text("${Answer[index~/4]}"):index%4==2?Text("${Question[index~/4]}"):Text("${Answer[index~/4]}"),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black,width: 1)
+          ),
+        );
+      })*/
