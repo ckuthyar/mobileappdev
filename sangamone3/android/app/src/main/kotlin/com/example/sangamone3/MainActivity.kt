252b95a2febc30e3c9a7ad43a9a1381f43c9c3a6
Shashank.S <personal.shashanks@outlook.com>
2025-02-20 23:22:21 +0530

    sangamone3

--- sangamone3/android/app/src/main/kotlin/com/example/sangamone3/MainActivity.kt
+++ sangamone3/android/app/src/main/kotlin/com/example/sangamone3/MainActivity.kt
@@ -1,51 +1,68 @@
 package com.example.sangamone3
 
 import android.Manifest
+import android.database.Cursor
 import android.os.Build
 import android.os.Bundle
+import android.provider.Telephony
 import android.telephony.SmsManager
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
+import androidx.core.content.PermissionChecker
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-
 class MainActivity: FlutterActivity(){
 
     private val channel = "com.example.sangamone3/sms"
-    private val GET_PERMISSION_CODE = 101
+    private val SEND_SMS_PERMISSION = 100
+    private val READ_SMS_PERMISSION = 101
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
         MethodChannel(flutterEngine.dartExecutor,channel).setMethodCallHandler { call, result ->
             when(call.method){
-                "sms" -> {
-                    val phoneNumber:String=""
-                    val text:String=""
-                    sendSms(phoneNumber,text)
-                    result.success("Message Sent")
+                "smsSend"->{
+                    val number = call.argument<String>("Phonenumber")
+                    val text = call.argument<String>("Text")
+                    if(number!=null && text!=null){
+                        sendSMS(number,text)
+                    }
+                }
+                "readSms"->{
+                    readSMS()
+                    result.success(readSMS())
                 }
             }
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        requestSmsPermission()
+    private fun requestSmsSend(){
+        if(ContextCompat.checkSelfPermission(context,Manifest.permission.SEND_SMS)!=PermissionChecker.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS,Manifest.permission.READ_SMS),SEND_SMS_PERMISSION)
+        }
     }
 
-    private fun requestSmsPermission(){
-        if(ContextCompat.checkSelfPermission(context,Manifest.permission.SEND_SMS)== PERMISSION_GRANTED){
-            Toast.makeText(context,"Permission already Granted",Toast.LENGTH_SHORT).show()
-        }
-        else{
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS),GET_PERMISSION_CODE)
+    private fun requestSmsRead(){
+        if(ContextCompat.checkSelfPermission(context,Manifest.permission.READ_SMS)!=PermissionChecker.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS,Manifest.permission.RECEIVE_SMS),READ_SMS_PERMISSION)
         }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PermissionChecker.PERMISSION_GRANTED) {
+            requestSmsSend()
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PermissionChecker.PERMISSION_GRANTED) {
+            requestSmsRead()
+        }
+    }
+
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -53,23 +70,47 @@ class MainActivity: FlutterActivity(){
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(grantResults.isEmpty() && grantResults[0]== PERMISSION_GRANTED){
-            Toast.makeText(context,"Permission Granted",Toast.LENGTH_SHORT).show()
-        }
-        else{
-            Toast.makeText(context,"Permission Not Granted",Toast.LENGTH_SHORT).show()
+        when(requestCode){
+            SEND_SMS_PERMISSION->{
+                if(grantResults.isEmpty()){
+                    Toast.makeText(context,"Permission not Granted",Toast.LENGTH_SHORT).show()
+                }
+            }
+            READ_SMS_PERMISSION->{
+                if(grantResults.isEmpty()){
+                    Toast.makeText(context,"Permission not Granted",Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
-    private fun sendSms(phoneNumber: String,text:String){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val smsManager: SmsManager = getSystemService(SmsManager::class.java) as SmsManager
+    private fun sendSMS(phoneNumber:String,text:String){
+        val smsManager : SmsManager
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            smsManager  = getSystemService(SmsManager::class.java)
             smsManager.sendTextMessage(phoneNumber,null,text,null,null)
-        } else {
-            Toast.makeText(context,"Android version Not satisfied",Toast.LENGTH_SHORT).show()
+            println("My Phone version is ${Build.VERSION.SDK_INT}")
         }
+        else{
+            Toast.makeText(context,"Android Version not support",Toast.LENGTH_SHORT).show()
+        }
+    }
 
+    private fun readSMS():List<Map<Any,Any>>{
+        val list1 = mutableListOf<Map<Any,Any>>()
+        val cursor : Cursor? = contentResolver.query(Telephony.Sms.CONTENT_URI,null,null,null,"date DESC")
+        cursor?.use {
+            while (cursor.moveToNext()){
+                val map1 = mutableMapOf<Any,Any>()
+                for (i in 0 until it.columnCount){
+                    map1[it.getColumnName(i)]=it.getString(i)?:""
+                }
+                list1.add(map1)
+            }
+        }
+        cursor?.close()
+
+        return list1
     }
 
 }
-
--- sangamone3/lib/screen1.dart
+++ sangamone3/lib/screen1.dart
@@ -21,29 +21,54 @@ class App1 extends StatefulWidget {
 
 class _App1State extends State<App1> {
 
-
-
   final channel = MethodChannel("com.example.sangamone3/sms");
   var print1="";
 
   getprint() async{
-    var data = await channel.invokeMethod("sms");
-    setState(() {
-      print1=data;
-    });
+    await channel.invokeMethod("smsSend",{"Phonenumber":tec1.text,"Text":tec2.text});
   }
-  
+
+  receive() async{
+    var data = await channel.invokeMethod("readSms");
+    return data;
+  }
   @override
   void initState() {
-    getprint();
+    receive();
     super.initState();
   }
+
+  TextEditingController tec1 = TextEditingController();
+  TextEditingController tec2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(print1),
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            TextField(
+              controller: tec1,
+            ),
+            TextField(
+              controller: tec2,
+            ),
+            ElevatedButton(onPressed: (){
+              getprint();
+            }, child: Text("Send SMS")),
+            
+            FutureBuilder(future: receive(), builder: (context,AsyncSnapshot<List<dynamic>> snapshot){
+              var data = snapshot.data;
+              return ListView.builder(physics: NeverScrollableScrollPhysics(),shrinkWrap: true,itemCount: 20,itemBuilder: (context,index){
+                return ListTile(
+                  title: Text("${data?[index]["address"]}"),
+                  subtitle: Text("${data?[index]["body"]}"),
+                );
+              });
+            })
+          ],
+        ),
       ),
     );
   }
